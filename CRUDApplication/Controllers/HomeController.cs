3525ac87a31b822d5fc17719e4393283574afec6
Sina Baradaran <sinabaradaran98@gmail.com>
2024-05-21 10:55:50 +0330

    Setup Contacts Manger CRUD Application

---
+++ CRUDApplication/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
