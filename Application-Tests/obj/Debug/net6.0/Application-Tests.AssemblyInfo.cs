29f281dc195f4fb525064fe9f3460c63d6ac074f
Sina Baradaran <sinabaradaran98@gmail.com>
2024-05-26 14:03:48 +0330

    some changes

--- Application-Tests/obj/Debug/net6.0/Application-Tests.AssemblyInfo.cs
+++ Application-Tests/obj/Debug/net6.0/Application-Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c897e6ccb71a282a9df78cf1f1b5f791c012d0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e056c62bf4dbe5551e1e1fdc16415716daf348ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
