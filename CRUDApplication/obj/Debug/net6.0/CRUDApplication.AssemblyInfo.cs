52bb3f624b193faff000d26450f13bca762ed7a4
Sina Baradaran <sinabaradaran98@gmail.com>
2024-05-30 21:02:43 +0330

    Use image tag helper and disable broser caching for log

--- CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
+++ CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a796eca25befec3de5b83292a0ace6bc993eecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68180ae1ea9ec582ed4d7f4b425891deb23194b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
