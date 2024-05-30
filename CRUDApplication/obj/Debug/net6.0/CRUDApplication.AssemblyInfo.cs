ce3ce2067bfb8ff04629d2bda6e6fca532ab8624
Sina Baradaran <sinabaradaran98@gmail.com>
2024-05-30 14:42:13 +0330

    apply tag helpers for select options and lables

--- CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
+++ CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043cbb48f63dc68a262368cad434c23941b7b1e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6288c27fc31a9f63c4e1a6fa4d034b484d3e2f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
