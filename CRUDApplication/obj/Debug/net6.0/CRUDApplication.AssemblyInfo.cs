a981f14bfdd8cf3602bc3217cdfae13f0db079f3
Sina Baradaran <sinabaradaran98@gmail.com>
2024-06-02 00:18:49 +0330

    some changes

--- CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
+++ CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6e231f5a104ea6ff08dacc90b9b61d8e800dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f75b96ea09db277aa7c39e57637a56b44a4f2f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
