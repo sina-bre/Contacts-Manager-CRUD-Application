df6e231f5a104ea6ff08dacc90b9b61d8e800dc7
Sina Baradaran <sinabaradaran98@gmail.com>
2024-06-01 21:39:36 +0330

    adding mock data for ef core

--- Entities/PersonsDBContext.cs
+++ Entities/PersonsDBContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Text.Json;
 namespace Entities
 {
     public class PersonsDBContext : DbContext
@@ -12,10 +14,44 @@ namespace Entities
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Country>().ToTable("Countries");
-
             modelBuilder.Entity<Person>().ToTable("Persons");
-        }
 
-        ModelBuilder
+
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            string? assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
+            if (assemblyDirectory is null)
+                throw new ArgumentNullException(nameof(assemblyDirectory));
+            DirectoryInfo? projectDirectory = Directory.GetParent(assemblyDirectory)?.Parent?.Parent?.Parent;
+            if (projectDirectory is null)
+            {
+                throw new DirectoryNotFoundException($"Project directory was not found.");
+            }
+            string countriesFilePath = Path.Combine(projectDirectory.FullName, "Entities", "mockData", "Countries.json");
+            if (!File.Exists(countriesFilePath))
+            {
+                throw new FileNotFoundException($"The file '{countriesFilePath}' was not found.");
+            }
+            string countriesJson = File.ReadAllText(countriesFilePath);
+            List<Country>? countries =
+           JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            if (countries is not null)
+                foreach (Country country in countries)
+                    modelBuilder.Entity<Country>().HasData(country);
+
+
+            string personsFilePath = Path.Combine(projectDirectory.FullName, "Entities", "mockData", "Countries.json");
+            if (!File.Exists(personsFilePath))
+            {
+                throw new FileNotFoundException($"The file '{personsFilePath}' was not found.");
+            }
+            string personsJson = File.ReadAllText(personsFilePath);
+            List<Person>? persons =
+           JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+            if (persons is not null)
+                foreach (Person person in persons)
+                    modelBuilder.Entity<Person>().HasData(person);
+        }
     }
 }
--- Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243faf5b63b52d47bf7f96c0593f4aa74ca140c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595a0ddf65a26cc9fa21f96f434f4075e65f7534")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
