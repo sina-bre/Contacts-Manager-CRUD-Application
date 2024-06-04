2f5b2eddfcce242932a472e0238ec9d445df50b7
Sina Baradaran <sinabaradaran98@gmail.com>
2024-06-04 19:59:47 +0330

    Work with some fluent api

--- Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382fc50aec5829ac29924ecba846037d1ba69390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7548770a39ae66d174caba8013cfe4e4e6093a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
