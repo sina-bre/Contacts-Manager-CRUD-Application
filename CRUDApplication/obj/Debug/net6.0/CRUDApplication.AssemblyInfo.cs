27a81a4b75023d35754767442cb16d36e4ea0212
Sina Baradaran <sinabaradaran98@gmail.com>
2024-06-09 00:11:04 +0330

    fix some css problems for pdf file

--- CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
+++ CRUDApplication/obj/Debug/net6.0/CRUDApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a9754b9a806b718d1f80a839a1a248b3e7725d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c9b8bc63df573d75e1117a9683b26e47d4da62")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
